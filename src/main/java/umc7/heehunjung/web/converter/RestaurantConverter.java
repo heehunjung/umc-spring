63e07cea2329a13fc971c82bbd465b123b0fc3db
heehun <slrkqk2@naver.com>
2024-11-17 20:07:15 +0900

    week-8 , mission-1

---
+++ src/main/java/umc7/heehunjung/web/converter/RestaurantConverter.java
@@ -0,0 +1,25 @@
+package umc7.heehunjung.web.converter;
+
+import umc7.heehunjung.domain.Region;
+import umc7.heehunjung.domain.Restaurant;
+import umc7.heehunjung.web.dto.RestaurantRequestDto;
+import umc7.heehunjung.web.dto.RestaurantResponseDto;
+
+public class RestaurantConverter {
+
+    public static RestaurantResponseDto.addDTO toAddDTO(Restaurant restaurant) {
+        return RestaurantResponseDto.addDTO.builder()
+                .restaurantId(restaurant.getId())
+                .createAt(restaurant.getCreatedAt())
+                .build();
+    }
+
+    public static Restaurant toRestaurant(RestaurantRequestDto.addDto request, Region region) {
+        return Restaurant.builder()
+                .region(region)
+                .name(request.getName())
+                .address(request.getAddress())
+                .content(request.getContent())
+                .build();
+    }
+}
